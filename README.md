ade861827087aaa60d69f6804be2151a65ee4894
Akagi201 <akagi201@gmail.com>
2015-03-22 21:31:31 +0800

    add ex01

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learning-python
 notes and codes while learning python
+
+## version management
+* pip
+* virtualenv
---
+++ lpthw/ex01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#ex01: A good first program
+
+print("Hello World!")
+print("Hello Again")
+print("I like Typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('This is another line!')
