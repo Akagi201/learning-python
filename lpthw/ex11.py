c115dc4fb11f79dd641f013c945ad645246a5418
Akagi201 <akagi201@gmail.com>
2015-03-25 16:11:50 +0800

    add ex11

---
+++ lpthw/ex11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Exercise 11: Asking Questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
