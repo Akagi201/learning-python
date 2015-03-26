46b617e4cc9a12162c88e02557491d3255c868be
Akagi201 <akagi201@gmail.com>
2015-03-26 18:59:23 +0800

    add ex28

---
+++ lpthw/ex28.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Exercise 28: Boolean Practice
+
+print True and True
+print False and True
+print 1 == 1 and 2 == 1
+print "test" == "test"
+print 1 == 1 or 2 != 1
+print True and 1 == 1
+print False and 0 != 0
+print True or 1 == 1
+print "test" == "testing"
+print 1 != 0 and 2 == 1
+print "test" != "testing"
+print "test" == 1
+print not (True and False)
+print not (1 == 1 and 0 != 1)
+print not (10 == 1 or 1000 == 1000)
+print not (1 != 10 or 3 == 4)
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+print 1 == 1 and (not ("testing" == 1 or 1 == 0))
+print "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+print 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+print "test" and "test"
+print False and 1
+print True and 1
