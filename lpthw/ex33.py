6d77dcbb01262a9518abdec1de1ed2278b6768f5
Akagi201 <akagi201@gmail.com>
2015-03-27 19:18:07 +0800

    add ex33

---
+++ lpthw/ex33.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Exercise 33: While Loops
+
+i = 0
+numbers = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+
+print "The numbers: "
+
+for num in numbers:
+    print num
