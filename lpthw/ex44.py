a4805fc4406fef30dafd968a5f81998a49ab4c70
Akagi201 <akagi201@gmail.com>
2015-04-04 22:31:49 +0800

    add ex44

---
+++ lpthw/ex44.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Exercise 44: Inheritance Versus Composition
+
+class Other(object):
+
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
