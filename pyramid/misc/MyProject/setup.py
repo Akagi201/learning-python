4731fe556a81fc741872e5856ee4ae69f9597ff9
Akagi201 <akagi201@gmail.com>
2015-05-02 21:00:36 +0800

    add pyramid chapter 3

---
+++ pyramid/misc/MyProject/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='MyProject',
+      version='0.0',
+      description='MyProject',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='myproject',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = myproject:main
+      [console_scripts]
+      initialize_MyProject_db = myproject.scripts.initializedb:main
+      """,
+      )
