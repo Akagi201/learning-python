5b4667aa226480964d57ba457beca00e95e3835a
Akagi201 <akagi201@gmail.com>
2015-06-29 22:33:04 +0800

    add stackoverflow_spider

---
+++ scrapy/stackoverflow_spider.py
@@ -0,0 +1,21 @@
+
+# scrapy runspider stackoverflow_spider.py -o top-stackoverflow-questions.json
+import scrapy
+
+class StackOverflowSpider(scrapy.Spider):
+    name = 'stackoverflow'
+    start_urls = ['http://stackoverflow.com/questions?sort=votes']
+
+    def parse(self, response):
+        for href in response.css('.question-summary h3 a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_question)
+
+    def parse_question(self, response):
+        yield {
+            'title': response.css('h1 a::text').extract()[0],
+            'votes': response.css('.question .vote-count-post::text').extract()[0],
+            'body': response.css('.question .post-text').extract()[0],
+            'tags': response.css('.question .post-tags::text').extract(),
+            'link': response.url,
+        }
