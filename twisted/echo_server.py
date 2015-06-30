af5d6879ee2477cab4fb9f2fcbbb6a105c0e33f1
Akagi201 <akagi201@gmail.com>
2015-06-30 22:23:08 +0800

    twisted: add echo_server

---
+++ twisted/echo_server.py
@@ -0,0 +1,16 @@
+
+# A TCP server that echoes back everything that's written to it
+# nc localhost 1234
+
+from twisted.internet import protocol, reactor, endpoints
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()
