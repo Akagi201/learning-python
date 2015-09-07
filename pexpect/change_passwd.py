02a43a9f3f7f3a7e92bdcf7a3e0b6d70e7091c0c
Akagi201 <akagi201@gmail.com>
2015-09-07 20:08:09 +0800

    add pexpect examples

---
+++ pexpect/change_passwd.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+# -*- coding: utf-8 -*-
+
+# 普通用户通过脚本来修改自己的密码
+# passwd –-stdin 参数只有 root 用户可以执行
+
+import pexpect
+
+# (current) UNIX password:
+def change_password(old_passwd, new_passwd):
+    child = pexpect.spawn('passwd')
+    child.expect('(current).*:')
+    child.sendline(old_passwd)
+    child.expect('New password.*:')
+    child.sendline(new_passwd)
+    child.expect('Retype new password.*:')
+    child.sendline(new_passwd)
+
+def change_password2(old_passwd, new_passwd):
+    child = pexpect.spawn('passwd')
+    child.sendline('passwd')
+    i = child.expect(['[Oo]ld [Pp]assword', '.current.*password', '[Nn]ew [Pp]assword'])
+    # Root does not require old password, so it gets to bypass the next step.
+    if i == 0 or i == 1:
+        child.sendline(old_passwd)
+        child.expect('[Nn]ew [Pp]assword')
+    child.sendline(new_passwd)
+    i = child.expect(['[Nn]ew [Pp]assword', '[Rr]etype', '[Rr]e-enter'])
+    if i == 0:
+        print('Host did not like new password. Here is what it said...')
+        print(child.before)
+        child.send(chr(3)) # Ctrl-C
+        child.sendline('') # This should tell remote passwd command to quit.
+        return
+    child.sendline(new_passwd)
+
+def main():
+    change_password2('howcute121', 'howcute222')
+
+if __name__ == '__main__':
+    main()
---
+++ pexpect/test_pexpect.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: test_pexpect.py
+
+import pexpect
+
+if __name__ == '__main__':
+    user = 'mm'
+    ip = '192.168.2.8'
+    mypassword = 'xxxx'
+
+    print user
+    child = pexpect.spawn('ssh %s@%s' % (user, ip))
+    # child.expect('password:')
+    child.sendline(mypassword)
+
+    child.expect('$')
+    # child.sendline('sudo -s')
+    # child.expect(':')
+    # child.sendline(mypassword)
+    # child.expect('#')
+    # child.sendline('ls -la')
+    # child.expect('#')
+    child.sendline('ls -al')
+    print child.before  # Print the result of the ls command.
+    child.sendline("echo '112' >> /home/mm/1.txt ")
+    child.interact()  # Give control of the child to the user.
+
+    print("lost")
