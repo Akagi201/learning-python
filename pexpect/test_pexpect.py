6851cc0bef5923941f5049ef3c0339875033245f
Akagi201 <akagi201@gmail.com>
2015-09-08 08:44:35 +0800

    update test_pexpect

--- pexpect/test_pexpect.py
+++ pexpect/test_pexpect.py
@@ -9,21 +9,23 @@ if __name__ == '__main__':
     ip = '192.168.2.8'
     mypassword = 'xxxx'
 
-    print user
-    child = pexpect.spawn('ssh %s@%s' % (user, ip))
+    # print user
+    # child = pexpect.spawn('ssh %s@%s' % (user, ip))
     # child.expect('password:')
-    child.sendline(mypassword)
+    # child.sendline(mypassword)
 
-    child.expect('$')
+    # child.expect('$')
     # child.sendline('sudo -s')
     # child.expect(':')
     # child.sendline(mypassword)
     # child.expect('#')
     # child.sendline('ls -la')
     # child.expect('#')
-    child.sendline('ls -al')
+    child = pexpect.spawn("ls -al")
+    # child.sendline('ls -al')
+    child.expect(pexpect.EOF)
     print child.before  # Print the result of the ls command.
-    child.sendline("echo '112' >> /home/mm/1.txt ")
+    # child.sendline("echo '112' >> /home/mm/1.txt ")
     child.interact()  # Give control of the child to the user.
 
     print("lost")
