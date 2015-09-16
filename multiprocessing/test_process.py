fa381b18fa4d836a06a91b280e9ebf144bc068ce
Akagi201 <akagi201@gmail.com>
2015-09-16 09:43:39 +0800

    add test_process

---
+++ multiprocessing/test_process.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from multiprocessing import Process
+
+def f(name):
+    print("hello", name)
+
+if __name__ == '__main__':
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
