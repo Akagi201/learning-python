ca788c183cd6539723a6ac73a6c600c506a571b8
Akagi201 <akagi201@gmail.com>
2015-09-16 10:01:27 +0800

    add test_pipe and test_queue

---
+++ multiprocessing/test_pipe.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from multiprocessing import Process, Pipe
+
+def f(conn):
+    conn.send([42, None, 'hello'])
+    conn.close()
+
+if __name__ == '__main__':
+    parent_conn, child_conn = Pipe()
+    p = Process(target=f, args=(child_conn,))
+    p.start()
+    print(parent_conn.recv()) # prints "[42, None, 'hello']"
---
+++ multiprocessing/test_queue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from multiprocessing import Process, Queue
+
+def f(q):
+    q.put([42, None, 'hello'])
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    print(q.get()) # prints "[42, None, 'hello']"
+    p.join()
