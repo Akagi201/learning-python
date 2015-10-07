15eb756c31c33ecb351fba99e02036049d453197
Akagi201 <akagi201@gmail.com>
2015-10-07 15:14:10 +0800

    add some tests

---
+++ flask/flask-socketio/hello.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
