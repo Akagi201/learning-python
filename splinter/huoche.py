f165df03b3012d18a81bf563aa55e48ec8fda588
Akagi201 <akagi201@gmail.com>
2015-12-09 11:33:53 +0800

    splinter: update huoche

--- splinter/huoche.py
+++ splinter/huoche.py
@@ -36,8 +36,11 @@ def login():
     b.fill("userDTO.password", passwd)
     sleep(1)
     print u"等待验证码，自行输入..."
-    sleep(20)
-
+    while True:
+        if b.url != initmy_url:
+            sleep(1)
+        else:
+            break
 
 def huoche():
     global b
@@ -61,22 +64,34 @@ def huoche():
         b.cookies.add({"_jc_save_fromDate": dtime})
         b.reload()
 
-        # 点击查询
-        b.find_by_text(u"查询").click()
+        sleep(2)
 
         count = 0
         # 循环点击预订
         if order != 0:
             while b.url == ticket_url:
-                b.find_by_text(u"预订")[order - 1].click()
-                # b.find_by_text(pa)[1].click()
+                b.find_by_text(u"查询").click()
+                count +=1
+                print u"循环点击查询... 第 %s 次" % count
+                sleep(1)
+                try:
+                    b.find_by_text(u"预订")[order - 1].click()
+                except:
+                    print u"还没开始预订"
+                    continue
         else:
             while b.url == ticket_url:
+                b.find_by_text(u"查询").click()
                 count += 1
-                print u"循环点击预订... 第 %s 次" % count
-                for i in b.find_by_text(u"预订"):
-                    i.click()
-                    # b.find_by_text(pa)[1].click()
+                print u"循环点击查询... 第 %s 次" % count
+                sleep(1)
+                try:
+                    for i in b.find_by_text(u"预订"):
+                        i.click()
+                except:
+                    print u"还没开始预订"
+                    continue
+        sleep(1)
         b.find_by_text(pa)[1].click()
         print  u"能做的都做了.....不再对浏览器进行任何操作"
     except Exception as e:
