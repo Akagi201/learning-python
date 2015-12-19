bb8ecc5ebb078c0119e74ac860fe4a329b29a1d5
Akagi201 <akagi201@gmail.com>
2015-12-19 21:41:25 +0800

    pyglet: add image_viewer

---
+++ pyglet/image_viewer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pyglet
+
+window = pyglet.window.Window()
+image = pyglet.resource.image('kitten.jpg')
+
+
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0, 0)
+
+
+pyglet.app.run()
