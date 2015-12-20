27db99e09df0eb35d9dc6740d2fe699425887921
Akagi201 <akagi201@gmail.com>
2015-12-20 12:33:41 +0800

    pyglet: add play_mp3 and avbin_test

---
+++ pyglet/avbin_test.py
@@ -0,0 +1,4 @@
+import pyglet
+print(pyglet.version)
+print(pyglet.media.have_avbin)
+print(pyglet.media.avbin.get_version())
---
+++ pyglet/play_mp3.py
@@ -0,0 +1,6 @@
+import pyglet
+
+music = pyglet.resource.media('a_little_story.wav')
+music.play()
+
+pyglet.app.run()
